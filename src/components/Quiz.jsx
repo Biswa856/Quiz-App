3ca3a49fcbdacca817b2e30f4bdacd4db484b9c8
biswajit.paul <biswajit.paul@infraplan.co.in>
2023-04-07 19:28:40 +0530

    animtaion added every place

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -10,42 +10,101 @@ export default function Quiz() {
     )
 }
 
-const Box = ({ currentQuiz,next }) => {
-    const { quizObj, correct, setCorrect, setExit,setAttempt,attempt } = useContext(QuizContext);
+const Box = ({ currentQuiz, next }) => {
+    const { quizObj, correct, setCorrect, setExit, setAttempt, attempt } = useContext(QuizContext);
     const [ans, setAns] = useState("");
 
     const saveHandler = () => {
-        if(quizObj[currentQuiz].correctIndex === ans){
+        if (quizObj[currentQuiz].correctIndex === ans) {
             setCorrect(correct + 1);
         }
         setAns('');
-        if((currentQuiz+1 )=== quizObj.length){
+        if ((currentQuiz + 1) === quizObj.length) {
             setExit(true)
-        }else{
-            next(currentQuiz+1);
+        } else {
+            next(currentQuiz + 1);
         }
 
-        setAttempt(attempt+1)
-       
+        setAttempt(attempt + 1)
+
     }
 
     return (
-        <div className='w-[50%] min-h-[250px] shadow-lg rounded-md overflow-hidden p-5'>
-            <div className='p-2 text-3xl' >{currentQuiz+1}) {quizObj[currentQuiz]?.question}</div>
-            <div className='grid grid-cols-2 mt-3'>
-                <div className={`p-2 border ${ans === "a" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400 
-                hover:text-white duration-300 cursor-pointer`} onClick={()=>setAns("a")}>a. {quizObj[currentQuiz]?.a}</div>
-                <div className={`p-2 border ${ans === "b" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400
-                 hover:text-white duration-300 cursor-pointer`} onClick={()=>setAns("b")}>b. {quizObj[currentQuiz]?.b}</div>
-                <div className={`p-2 border ${ans === "c" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400
-                 hover:text-white duration-300 cursor-pointer`} onClick={()=>setAns("c")}>c. {quizObj[currentQuiz]?.c}</div>
-                <div className={`p-2 border ${ans === "d" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400
-                 hover:text-white duration-300 cursor-pointer`} onClick={()=>setAns("d")}>d. {quizObj[currentQuiz]?.d}</div>
-            </div>
-            <div className='flex justify-between p-4 mt-5'>
-                <div className='h-[30px] border border-blue-700 rounded px-3 hover:bg-blue-500 hover:text-white cursor-pointer' onClick={()=>setAns("")}>Reset</div>
-                <div className='h-[30px] border border-green-700 rounded px-3 hover:bg-green-600 hover:text-white cursor-pointer' onClick={saveHandler}>Save & Next</div>
-                <div className='h-[30px] border border-red-600 rounded px-3 hover:bg-red-500 hover:text-white cursor-pointer'onClick={()=>setExit(true)}>Exit</div>
+        <div className='animation-body'>
+            <div className='container'>
+                <div className='bubbles'>
+                    <span style={{ '--i': 10 }}></span>
+                    <span style={{ '--i': 12 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 25 }}></span>
+                    <span style={{ '--i': 20 }}></span>
+                    <span style={{ '--i': 11 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 14 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 23 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 19 }}></span>
+                    <span style={{ '--i': 10 }}></span>
+                    <span style={{ '--i': 23 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 21 }}></span>
+                    <span style={{ '--i': 25 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 12 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 29 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 14 }}></span>
+                    <span style={{ '--i': 26 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 16 }}></span>
+                    <span style={{ '--i': 26 }}></span>
+                    <span style={{ '--i': 20 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 27 }}></span>
+                    <span style={{ '--i': 23 }}></span>
+                    <span style={{ '--i': 29 }}></span>
+                    <span style={{ '--i': 28 }}></span>
+                    <span style={{ '--i': 35 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 28 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 16 }}></span>
+                    <span style={{ '--i': 26 }}></span>
+                    <span style={{ '--i': 20 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 27 }}></span>
+                </div>
+                <div style={{ display: 'flex', justifyContent: "center", alignItems: 'center', height:'100vh' }}>
+                    <div className='w-[50%] min-h-[250px] shadow-lg rounded-md overflow-hidden p-5 bg-slate-100 absolute'>
+                        <div className='p-2 text-3xl' >{currentQuiz + 1}) {quizObj[currentQuiz]?.question}</div>
+                        <div className='grid grid-cols-2 mt-3'>
+                            <div className={`p-2 border ${ans === "a" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400 
+                hover:text-white duration-300 cursor-pointer`} onClick={() => setAns("a")}>a. {quizObj[currentQuiz]?.a}</div>
+                            <div className={`p-2 border ${ans === "b" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400
+                 hover:text-white duration-300 cursor-pointer`} onClick={() => setAns("b")}>b. {quizObj[currentQuiz]?.b}</div>
+                            <div className={`p-2 border ${ans === "c" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400
+                 hover:text-white duration-300 cursor-pointer`} onClick={() => setAns("c")}>c. {quizObj[currentQuiz]?.c}</div>
+                            <div className={`p-2 border ${ans === "d" ? 'bg-blue-500 text-white' : ''} hover:bg-blue-400
+                 hover:text-white duration-300 cursor-pointer`} onClick={() => setAns("d")}>d. {quizObj[currentQuiz]?.d}</div>
+                        </div>
+                        <div className='flex justify-between p-4 mt-5'>
+                            <div className='h-[30px] border border-blue-700 rounded px-3 hover:bg-blue-500 hover:text-white cursor-pointer' onClick={() => setAns("")}>Reset</div>
+                            <div className='h-[30px] border border-green-700 rounded px-3 hover:bg-green-600 hover:text-white cursor-pointer' onClick={saveHandler}>Save & Next</div>
+                            <div className='h-[30px] border border-red-600 rounded px-3 hover:bg-red-500 hover:text-white cursor-pointer' onClick={() => setExit(true)}>Exit</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -11,7 +11,61 @@ export default function Result() {
         setCorrect(0)
     }
   return (
-    <div className='w-full h-screen flex justify-center items-center bg-slate-50'>
+    <div className='animation-body'>
+            <div className='container'>
+                <div className='bubbles'>
+                    <span style={{ '--i': 10 }}></span>
+                    <span style={{ '--i': 12 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 25 }}></span>
+                    <span style={{ '--i': 20 }}></span>
+                    <span style={{ '--i': 11 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 14 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 23 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 19 }}></span>
+                    <span style={{ '--i': 10 }}></span>
+                    <span style={{ '--i': 23 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 21 }}></span>
+                    <span style={{ '--i': 25 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 15 }}></span>
+                    <span style={{ '--i': 12 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 29 }}></span>
+                    <span style={{ '--i': 30 }}></span>
+                    <span style={{ '--i': 14 }}></span>
+                    <span style={{ '--i': 26 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 16 }}></span>
+                    <span style={{ '--i': 26 }}></span>
+                    <span style={{ '--i': 20 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 27 }}></span>
+                    <span style={{ '--i': 23 }}></span>
+                    <span style={{ '--i': 29 }}></span>
+                    <span style={{ '--i': 28 }}></span>
+                    <span style={{ '--i': 35 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 28 }}></span>
+                    <span style={{ '--i': 13 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 16 }}></span>
+                    <span style={{ '--i': 26 }}></span>
+                    <span style={{ '--i': 20 }}></span>
+                    <span style={{ '--i': 18 }}></span>
+                    <span style={{ '--i': 27 }}></span>
+                </div>
+    <div className='w-full h-screen flex justify-center items-center bg-slate-50 ' style={{backgroundImage:"url('https://cdn.pixabay.com/photo/2019/10/09/06/06/gift-4536515_960_720.png')"}}>
         <div className='w-[50%] min-h-[250px] shadow-lg rounded-md overflow-hidden p-5 text-center text-2xl'>
             Report Card
 
@@ -22,6 +76,8 @@ export default function Result() {
         <button onClick={playAgain} className='border bg-red-500 mt-10 px-2 rounded hover:bg-rose-500 hover:text-white'>Play Again</button> 
         </div>
         
+    </div>
+    </div>
     </div>
   )
 }
